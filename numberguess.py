e27cf1407dedc2aa478a697971fa3d0316ac5646
Hulkmon001 <124511806+Hulkmon001@users.noreply.github.com>
2023-02-07 00:33:56 +0530

    Update numberguess.py

--- numberguess.py
+++ numberguess.py
@@ -1,7 +1,7 @@
 def guess():
     import random
-    guess = int(input("enter your guess in range 1 to 100: "))
-    robo_guess = random.randint(1, 100)
+    guess = int(input("enter your guess in range 1 to 1000: "))
+    robo_guess = random.randint(1, 1000)
 
     if guess == robo_guess:
         print("Your guess is right")
